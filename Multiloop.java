2087db47e27dbb8d5239666c6541914188f62af4
dheenadhayalan1502 <dheenadhayalan1502@gmail.com>
2026-02-12 08:58:21 +0530

    progamm about loops

---
+++ Multiloop.java
@@ -0,0 +1,17 @@
+class Multiloop
+{
+	public static void main(String[]args)
+	{
+		
+		int i=1;
+		int num=7;
+		int res=0;
+		do
+		{
+		   System.out.println("7 X"+i+" = "+(res=i*num));
+		   i++;
+         		
+		}
+		while(i<=10);
+	}
+}
